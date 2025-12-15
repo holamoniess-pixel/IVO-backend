49f2ec552d55384ed79804bf28f7863b5132c048
holamoniess-pixel <holamoniess@gmail.com>
2025-12-14 20:32:16 -0800

    Add job schema and model to jobs route

--- routes/jobs.js
+++ routes/jobs.js
@@ -32,3 +32,13 @@ router.get("/", async (req, res) => {
 });
 
 module.exports = router;
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema({
+  title: String,
+  company: String,
+  description: String
+}, { timestamps: true });
+
+module.exports = mongoose.model("Job", jobSchema);
+
